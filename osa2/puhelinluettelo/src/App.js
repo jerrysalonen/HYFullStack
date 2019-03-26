e0469d9f07aa6fcfed54c613fcb8e6027d9eef84
jerrysalonen <32097936+jerrysalonen@users.noreply.github.com>
2019-03-26 14:54:15 +0200

    Puhelinluettelo update

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,22 +3,21 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Search from './components/Search'
 import Add from './components/Add'
 import List from './components/List'
-import axios from 'axios'
+import contactHandler from './services/contactHandler'
 
 const App = () => {
-  const [persons, setPersons] = useState([])  
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameSearch, setNameSearch] = useState('')
-  const [showContacts, setShowContacts] = useState(true)
+  const [showContacts, setShowContacts] = useState([])
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('data fetched')
-        setPersons(response.data)
+    contactHandler.readAll()
+      .then(tempPersons => {
+        setPersons(tempPersons)
+        setShowContacts(tempPersons)
       })
   }, [])
 
@@ -32,29 +31,36 @@ const App = () => {
 
   const nameSearchListener = (event) => {
     setNameSearch(event.target.value)
-    console.log(nameSearch)
-    persons.forEach((person) => {
-      if (person.name.search(regex) >= 0) {
-        setShowContacts(false)
-      } else {
-        setShowContacts(true)
-      }
-    })
+    filter(event.target.value)
   }
 
-  let reg = "^" + nameSearch
-  let regex = new RegExp(reg, "ig")
-  const contactsToShow = showContacts
-    ? persons
-    : persons.filter(person => person.name.search(regex) >= 0)
+  const filter = (filterText) => {
+    let filterPersons = persons
+    filterPersons = filterPersons.filter((person) => {
+      let personName = person.name.toLowerCase()
+      return personName.indexOf(
+        filterText.toLowerCase()) !== -1
+    })
+    if (filterPersons.length === persons.length) {
+      setShowContacts(persons)
+    } else {
+      setShowContacts(filterPersons)
+    }
+  }
+
+  const update = (id, person) => {
+    contactHandler.updateNum(id, person)
+  }
 
   const addContact = (event) => {
     event.preventDefault()
     let nameExists = false
+    let tempPerson
 
     persons.forEach((contactTemp) => {
       if (contactTemp.name === newName) {
         nameExists = true
+        tempPerson = contactTemp
       }
     })
 
@@ -64,15 +70,53 @@ const App = () => {
         number: newNumber
       }
       setPersons(persons.concat(newContact))
+      contactHandler.addContact(newContact)
       setNewName('')
       setNewNumber('')
+
+      contactHandler.readAll()
+        .then(tempPersons => {
+          setPersons(tempPersons)
+          setShowContacts(tempPersons)
+        })
     } else {
-      alert(`${newName} on jo puhelinluettelossa!`)
+      let confirm = window.confirm(`${newName} on jo luettelossa, korvataanko vanha numero uudella?`)
+      if (confirm) {
+        const newContact = {
+          name: newName,
+          number: newNumber
+        }
+        let id = tempPerson.id
+        update(id, newContact)
+        setNewName('')
+        setNewNumber('')
+
+        contactHandler.readAll()
+          .then(tempPersons => {
+            setPersons(tempPersons)
+            setShowContacts(tempPersons)
+          })
+      }
     }
   }
 
-  const mapNames = () => contactsToShow.map(person =>
-    <p key={person.name}>{person.name} {person.number}</p>
+  const deleteContact = (person) => {
+    let confirm = window.confirm(`Poistetaanko ${person.name}?`)
+    if (confirm) {
+      contactHandler.deleteContact(person)
+      contactHandler.readAll()
+        .then(tempPersons => {
+          setPersons(tempPersons)
+          setShowContacts(tempPersons)
+        })
+    }
+  }
+
+  const mapNames = () => showContacts.map(person =>
+    <div key={person.name + "_div"}>
+      <p key={person.name}>{person.name} {person.number}</p>
+      <button onClick={() => deleteContact(person)} className="btn btn-primary" key={person.name + "_btn"}>Poista</button>
+    </div>
   )
 
   return (
@@ -83,11 +127,11 @@ const App = () => {
       <Search value={nameSearch} onChange={nameSearchListener} />
 
       <Add valueName={newName}
-          valueNumber={newNumber}
-          onChangeName={nameListener}
-          onChangeNumber={numberListener} 
-          onClick={addContact}
-        />
+        valueNumber={newNumber}
+        onChangeName={nameListener}
+        onChangeNumber={numberListener}
+        onClick={addContact}
+      />
 
       <List mapNames={mapNames()} />
 
